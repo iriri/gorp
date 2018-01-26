c10edd3c1d698f12c19c9d6a065d9f30ec0c482a
iriri <iri@konnichiwastevenspielbergde.su>
2018-01-25 23:01:02 -0800

    We concurrency now

--- gorp.go
+++ gorp.go
@@ -5,26 +5,44 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strings"
 )
 
-func main() {
-	r, err := regexp.Compile(os.Args[1])
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "invalid regex, probably\n")
-		panic(err)
-	}
-
-	f, err := os.Open(os.Args[2])
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "invalid filepath, probably\n")
-		panic(err)
-	}
-
-	s := bufio.NewScanner(bufio.NewReader(f))
-
+func search(r *regexp.Regexp, s *bufio.Scanner, c chan string) {
 	for s.Scan() {
 		if r.FindString(s.Text()) != "" {
-			fmt.Println(s.Text())
+			c <- s.Text()
+		}
+	}
+	close(c)
+}
+
+func main() {
+	r, err := regexp.Compile(strings.Replace(os.Args[1], "\\|", "|", -1))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "invalid regexp, probably\n")
+		panic(err)
+	}
+
+	var f *os.File
+	var scn *bufio.Scanner
+	cs := make([]chan string, len(os.Args)-2)
+	for i, s := range os.Args[2:] {
+		f, err = os.Open(s)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "invalid filepath, probably\n")
+			panic(err)
+		}
+		defer f.Close()
+
+		scn = bufio.NewScanner(bufio.NewReader(f))
+		cs[i] = make(chan string)
+		go search(r, scn, cs[i])
+	}
+
+	for _, c := range cs {
+		for s := range c {
+			fmt.Println(s)
 		}
 	}
 }
